4e2b5bda0b804f75efc2a97cce867f25eb2a710f
Max Aehle <max.aehle@scicomp.uni-kl.de>
2022-01-10 19:27:02 +0100

    Add script source-dep.sh
    to set paths after container startup

---
+++ software/scripts/source-dep.sh
@@ -0,0 +1,5 @@
+
+# Source this script inside the container to add paths.
+source $GATE_ROOT_DIR/geant4/install/bin/geant4.sh 
+source $GATE_ROOT_DIR/root-cern/install/bin/thisroot.sh 
+export PATH=$PATH:$GATE_ROOT_DIR/gate/bin
