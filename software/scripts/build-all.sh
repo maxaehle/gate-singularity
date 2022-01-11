60304bbb9f67695d5ca909a04dd741418b6472bb
Max Aehle <aehle@rhrk.uni-kl.de>
2022-01-11 17:24:07 +0100

    Execution instead of sourcing in build-all.sh

--- software/scripts/build-all.sh
+++ software/scripts/build-all.sh
@@ -9,14 +9,14 @@
 # - geant4/configure.sh depends on clhep/build.sh
 # - gate/configure.sh depends on geant4/build.sh and root-cern/build.sh
 
-. $GATE_ROOT_DIR/clhep/configure.sh
-. $GATE_ROOT_DIR/clhep/build.sh
-. $GATE_ROOT_DIR/geant4/configure.sh
-. $GATE_ROOT_DIR/geant4/build.sh
-. $GATE_ROOT_DIR/ITK/configure.sh
-. $GATE_ROOT_DIR/ITK/build.sh
-. $GATE_ROOT_DIR/root-cern/configure.sh
-. $GATE_ROOT_DIR/root-cern/build.sh
-. $GATE_ROOT_DIR/gate/configure.sh
-. $GATE_ROOT_DIR/gate/build.sh
+$GATE_ROOT_DIR/clhep/configure.sh
+$GATE_ROOT_DIR/clhep/build.sh
+$GATE_ROOT_DIR/geant4/configure.sh
+$GATE_ROOT_DIR/geant4/build.sh
+$GATE_ROOT_DIR/ITK/configure.sh
+$GATE_ROOT_DIR/ITK/build.sh
+$GATE_ROOT_DIR/root-cern/configure.sh
+$GATE_ROOT_DIR/root-cern/build.sh
+$GATE_ROOT_DIR/gate/configure.sh
+$GATE_ROOT_DIR/gate/build.sh
 
