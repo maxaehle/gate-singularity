041b8d7247e176d13ae56505893c7b4439859555
Max Aehle <max.aehle@scicomp.uni-kl.de>
2022-02-03 15:27:44 +0100

    Source source-dep.sh at container start-up
    in the %runscript section of the Singularity definition file.
    When running the container for the first time, Geant4 and ROOT
    have not been built yet and so their "source scripts" are not
    sourced.

--- software/scripts/source-dep.sh
+++ software/scripts/source-dep.sh
@@ -1,5 +1,11 @@
-
 # Source this script inside the container to add paths.
-source $GATE_ROOT_DIR/geant4/install/bin/geant4.sh 
-source $GATE_ROOT_DIR/root-cern/install/bin/thisroot.sh 
+# You need to source this script again after compiling Geant4 
+# and/or ROOT.
+
+if [[ -f $GATE_ROOT_DIR/geant4/install/bin/geant4.sh ]]; then
+  source $GATE_ROOT_DIR/geant4/install/bin/geant4.sh 
+fi
+if [[ -f $GATE_ROOT_DIR/root-cern/install/bin/thisroot.sh ]]; then
+  source $GATE_ROOT_DIR/root-cern/install/bin/thisroot.sh 
+fi
 export PATH=$PATH:$GATE_ROOT_DIR/gate/bin
