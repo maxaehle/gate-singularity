566ea352b4d9f03a45ad53e3c85cafca55e670a5
Max Aehle <max.aehle@scicomp.uni-kl.de>
2022-02-03 16:39:33 +0100

    Changes to run the DTC toolkit in the container

--- software/scripts/pre-configure.sh
+++ software/scripts/pre-configure.sh
@@ -1,4 +1,4 @@
 # This file is source'd in the beginning of every /software/<project>/configure.sh.
-export CMAKE_BUILD_TYPE=Debug
+export CMAKE_BUILD_TYPE=Release
 source $GATE_ROOT_DIR/scripts/versions.sh
 export PATH=$GATE_ROOT_DIR/cmake-3.18.4-Linux-x86_64/bin:$PATH
