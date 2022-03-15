71472a3eee7c7f3015eaf782534ba23e2385f0fa
Max Aehle <max.aehle@scicomp.uni-kl.de>
2022-03-15 12:14:11 +0100

    Adapt CXX standard for ROOT

--- software/root-cern/configure.sh
+++ software/root-cern/configure.sh
@@ -5,5 +5,6 @@ source $GATE_ROOT_DIR/scripts/pre-configure.sh
 cd $GATE_ROOT_DIR/root-cern/bin && \
   cmake ../src/ -Dpython=OFF \
                   -DCMAKE_INSTALL_PREFIX=$PWD/../install \
-                  -DCMAKE_BUILD_TYPE=$CMAKE_BUILD_TYPE
+                  -DCMAKE_BUILD_TYPE=$CMAKE_BUILD_TYPE \
+                  -DCMAKE_CXX_STANDARD=17 
 
