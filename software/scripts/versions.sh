ebb189fa9777c50ce8278278c0ce3bb8aefe0e4c
Max Aehle <max.aehle@scicomp.uni-kl.de>
2024-01-30 12:54:52 +0100

    Rename adapted versions as 'vX.Y_latest', keep originals as 'vX.Y'

--- software/scripts/versions.sh
+++ software/scripts/versions.sh
@@ -1,4 +1,4 @@
-export Gate_Version=v9.2
+export Gate_Version=v9.2_latest
 
 export ITK_Version=master
 
@@ -8,6 +8,14 @@ v8.1)
   export ROOT_Version=v6-19-02
 # export ROOT_Version=v6-14-02 -> This version doesn't compile
   export CLHEP_Version=2.3.3.2
+  export Geant4_Version=v10.4.2
+
+  export USE_RTK=OFF
+  ;;
+
+v8.1_latest)
+  export ROOT_Version=v6-19-02
+  export CLHEP_Version=2.3.3.2
   export Geant4_Version=v10.4.3
 
   export USE_RTK=OFF
@@ -15,12 +23,17 @@ v8.1)
 
 v8.2)
   export ROOT_Version=v6-19-02
-  # export ROOT_Version=v6-14-06 -> This version doesn't compile
   export CLHEP_Version=2.4.1.0
   export Geant4_Version=v10.5.1
   ;;
   
 v9.0)
+  export ROOT_Version=v6-19-02
+  export CLHEP_Version=2.4.1.3
+  export Geant4_Version=v10.6.1
+  ;;
+
+v9.0_latest)
   export ROOT_Version=v6-19-02
   export CLHEP_Version=2.4.1.3
   export Geant4_Version=v10.6.3
@@ -29,10 +42,23 @@ v9.0)
 v9.1)
   export ROOT_Version=v6-19-02
   export CLHEP_Version=2.4.4.1
+  export Geant4_Version=v10.7.1
+  ;;
+
+v9.1_latest)
+  export ROOT_Version=v6-19-02
+  export CLHEP_Version=2.4.4.1
   export Geant4_Version=v10.7.4
   ;;
 
 v9.2)
+  export ROOT_Version=v6-24-06
+  export CLHEP_Version=2.4.5.1
+  export Geant4_Version=v11.0.0
+  #export ITK_Version=v5.2.0
+  ;;
+
+v9.2_latest)
   export ROOT_Version=v6-24-06
   export CLHEP_Version=2.4.5.1
   export Geant4_Version=v11.0.4
